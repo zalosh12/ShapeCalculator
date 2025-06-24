515dc1ba00d96c195adf1c594fbbb463e063131c
Elazar <e0548448478@gmail.com>
2025-06-24 16:44:23 +0300

    the class circle added

---
+++ circle.py
@@ -0,0 +1,10 @@
+from calculator import Shape
+from math import pi
+
+class Circle(Shape):
+    def __init__(self,radius):
+        super().__init__("Circle")
+        self.radius = radius
+
+    def get_area(self):
+        return pi * self.radius ** 2
