65a6cfabdfe742be00bcdd1c3bd73770aa7108fc
Elazar <e0548448478@gmail.com>
2025-06-24 16:39:23 +0300

    the class triangle added

---
+++ triangle.py
@@ -0,0 +1,9 @@
+from rectangle import Rectangle
+
+class Triangle(Rectangle):
+    def __init__(self,base,height):
+        super().__init__(base,height)
+        self.name = "Triangle"
+
+    def get_area(self):
+        return 0.5 * self.width * self.height
